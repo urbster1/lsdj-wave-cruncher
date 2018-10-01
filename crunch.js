615c92ba98b4ffa6e86ae2eeae3c09a3f365b7e3
urbster1 <urbster1@users.noreply.github.com>
2018-10-01 11:18:45 -0400

    Added --analyze flag
    Analyze waveform only, do not output .snt file

--- crunch.js
+++ crunch.js
@@ -9,11 +9,18 @@ flags.defineBoolean('normalize', false, 'Normalize the wave ?')
 flags.defineBoolean('channel', 0, 'Channel for data?')
 flags.defineBoolean('linear', false, 'Linear interpolation?')
 flags.defineBoolean('exp', false, 'Exponential interpolation?')
+flags.defineBoolean('analyze', false, 'Analyze pitch')
 flags.defineString('output', '', 'Output filename')
 
 // check usage
 if (process.argv.length < 4) {
-  console.log('Usage: crunch [SOUND.WAV] [NOTE|BASE FREQUENCY|auto] [--linear|--exp] [--normalize] [--channel=0] [--output=filename]')
+  console.log(`Usage: crunch [SOUND.WAV] [NOTE|BASE FREQUENCY|auto]
+  Optional flags
+  --linear|--exp: choose waveform interpolation
+  --normalize: normalize waveform prior to crunching
+  --channel=0: choose channel 0 or 1 for stereo wave file
+  --output=filename: provide output filename
+  --analyze: analyze waveform only, do not output .snt file`)
   process.exit(1)
 }
 // parse flags
@@ -30,19 +37,22 @@ var synthdata = cruncher(process.argv[2], process.argv[3], {
 })
 
 // log
+var analyze = flags.get('analyze')
 var output = flags.get('output')
 if (output.length < 1) {
 	var filename = path.basename(process.argv[2], path.extname(process.argv[2])) + '.snt'
 } else {
 	var filename = output
 }
-console.log("Saving data as " + filename + "...")
 // creating buffer
 var buf = Buffer.from(_.map(_.chunk(synthdata, 2), function (chunk) {
   return _.reduce(chunk, function(cur, oth) {
     return (cur << 4) + oth
   }, 0)
 }))
+
+if (!analyze) {
+console.log("Saving data as " + filename + "...")
 
 // save buff
 fs.writeFile(path.dirname(process.argv[2]) + "/" + filename, buf, function(err) {
@@ -54,6 +64,7 @@ fs.writeFile(path.dirname(process.argv[2]) + "/" + filename, buf, function(err) 
     console.log("Successfully output " + filename + "!")
   }
 })
+}
 
 // done
 console.log('Done!')
